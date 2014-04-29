46bf65e5646ade2a81fe19db96f4fec5f304118f
Srinidhi J <srinidhij.21@gmail.com>
2014-04-29 22:43:02 +0530

    BotClean Large

---
+++ ai/botcleanlarge/solution.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+from math import sqrt
+def next_move(posr, posc, dimh, dimw, board):
+    if board[posr][posc] == 'd':
+        print 'CLEAN'
+        return
+    i = 0
+    j = 0
+    dirtypos = []
+    while i < dimh:
+        j = 0
+        while j < dimw:
+            if board[i][j] == 'd':
+                dirtypos.append((i,j))
+            j += 1
+        i += 1
+    leastd = 99999
+    t = None
+    for d in dirtypos:
+        dist = sqrt((posr-d[0])**2 + (posc-d[1])**2)
+        if dist <= leastd:
+            leastd = dist
+            t = d
+
+    #print t
+    #print posr, posc
+
+    if (posr - t[0]) == 0:
+        direction = 1
+    elif (posc - t[1]) == 0:
+        direction = 0
+    else:
+        direction = 1 if abs(posr - t[0]) <= abs(posc - t[1]) else 0
+
+    #print direction
+    #print 'posc, t[1] = ', posc, t[1]
+    #print 'posr, t[0] = ', posr, t[0]
+
+    if direction == 1:
+        if posc > t[1]:
+            print 'LEFT'
+        else :
+            print 'RIGHT'
+    if direction == 0:
+        if posr > t[0]:
+            print 'UP'
+        else:
+            print 'DOWN'
+
+if __name__ == "__main__":
+    pos = [int(i) for i in raw_input().strip().split()]
+    dim = [int(i) for i in raw_input().strip().split()]
+    board = [[j for j in raw_input().strip()] for i in range(dim[0])]
+    next_move(pos[0], pos[1], dim[0], dim[1], board)
